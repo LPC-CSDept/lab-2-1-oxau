423df33b32f04ae043b1d86d81839fd443c7444c
oxau <notoriousact@gmail.com>
2023-01-31 17:32:16 -0800

    commit

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
-
+    original_price = int(200)
+    rate = int(0.20)
+    discount_amount = int (40)
+    final_sales_price = original_price - discount_amount
+    print (original_price)
+    print (discount_amount)
+    print (final_sales_price)
     pass
 
 
